ca9430474d99ad86768eeebc66575dae0529e701
hanX3 <hanxingchi@gmail.com>
2025-04-07 18:35:38 +0800

    all valid run

--- raw2hist/macro.cpp
+++ raw2hist/macro.cpp
@@ -1,15 +1,24 @@
 {
   gROOT->ProcessLine(".L get_hist_cali.cpp");
 
-  gROOT->ProcessLine("get_hist_cali(\"../rootfile/ge/data_C1_0472.root\", 472)");
-  gROOT->ProcessLine("get_hist_cali(\"../rootfile/ge/data_C1_0496.root\", 496)");
-  gROOT->ProcessLine("get_hist_cali(\"../rootfile/ge/data_C1_0519.root\", 519)");
-  gROOT->ProcessLine("get_hist_cali(\"../rootfile/ge/data_C1_0546.root\", 546)");
-  gROOT->ProcessLine("get_hist_cali(\"../rootfile/ge/data_C1_0576.root\", 576)");
-  gROOT->ProcessLine("get_hist_cali(\"../rootfile/ge/data_C1_0603.root\", 603)");
-  gROOT->ProcessLine("get_hist_cali(\"../rootfile/ge/data_C1_0634.root\", 634)");
-  gROOT->ProcessLine("get_hist_cali(\"../rootfile/ge/data_C1_0660.root\", 660)");
-  gROOT->ProcessLine("get_hist_cali(\"../rootfile/ge/data_C1_0691.root\", 691)");
+  ifstream fi("../pars/run.txt");
+  if(!fi){
+    cout << "can not open pars/run.txt" << endl;
+    return;
+  }
+
+  int run;
+  TString cmd;
+  while(1){
+    fi >> run;
+    if(!fi.good())  break;
+
+    cmd = TString::Format("get_hist_cali(\"../rootfile/ge/data_C1_%04d.root\", %d)", run, run);
+    cout << cmd << endl;
+    gROOT->ProcessLine(cmd.Data());
+  }
+
+  fi.close();
 
   gROOT->ProcessLine(".q");
 }

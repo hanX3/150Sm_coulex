a7457644ca5a4744802c57aa4efd4704cc9a4faf
hanX3 <hanxingchi@gmail.com>
2025-04-27 10:40:57 +0800

    tree2gr.cpp & batch_tree2gr.sh

---
+++ s3_fb_cor/batch_tree2gr.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+max_jobs=60
+
+while read run; do
+  root -b -q "tree2gr.cpp($run)" &
+
+  while (( $(jobs -p | wc -l) >= $max_jobs )); do
+    wait -n
+  done
+done < "../pars/run.txt"
+

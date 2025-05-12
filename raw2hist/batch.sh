8771687d3345a7241e7e2588911217ef603010cc
hanX3 <hanxingchi@gmail.com>
2025-05-12 19:59:03 +0800

    batch run

---
+++ raw2hist/batch.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+max_jobs=120
+
+mkdir -p out
+
+while read run; do
+  # root -b -q "macro_raw.cpp($run)" &
+  # root -b -q "macro_cali_ge.cpp($run)" &
+  # root -b -q "macro_cali_si.cpp($run)" &
+  root -b -q "macro_cali.cpp($run)" &
+
+  while (( $(jobs -p | wc -l) >= $max_jobs )); do
+    wait -n
+  done
+done < "../pars/run.txt"
+

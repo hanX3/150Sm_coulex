311cebf61e30ccbb0f9872fe08e29b8dfb92d08f
hanX3 <hanxingchi@gmail.com>
2025-05-13 15:26:26 +0800

    add draw_sample in it

--- raw2hist/batch.sh
+++ raw2hist/batch.sh
@@ -8,7 +8,8 @@ while read run; do
   # root -b -q "macro_raw.cpp($run)" &
   # root -b -q "macro_cali_ge.cpp($run)" &
   # root -b -q "macro_cali_si.cpp($run)" &
-  root -b -q "macro_cali.cpp($run)" &
+  # root -b -q "macro_cali.cpp($run)" &
+  root -b -q "draw_sample.cpp($run)" &
 
   while (( $(jobs -p | wc -l) >= $max_jobs )); do
     wait -n

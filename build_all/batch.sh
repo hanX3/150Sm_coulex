53680f5c087c7af3c33c4ac7c38fc83f1a2451be
hanX3 <hanxingchi@gmail.com>
2025-05-27 15:56:42 +0800

    update

--- build_all/batch.sh
+++ build_all/batch.sh
@@ -5,8 +5,10 @@ max_jobs=120
 mkdir -p out
 
 while read run; do
-  ./build $run > out/$run &
-  # root -b -q "macro.cpp($run)" &
+  # ./build $run > out/$run &
+  root -b -q "macro.cpp($run, 100)" &
+  root -b -q "macro.cpp($run, 200)" &
+  root -b -q "macro.cpp($run, 1000)" &
   # root -b -q "draw_sample.cpp($run)" &
 
   while (( $(jobs -p | wc -l) >= $max_jobs )); do
--- build_all/macro.cpp
+++ build_all/macro.cpp
@@ -1,10 +1,14 @@
 //
-void macro(int run)
+void macro(int run, int win)
 {
   gROOT->ProcessLine(".L raw.cpp");
   gROOT->ProcessLine(".L b2hist.cpp");
 
-  TString cmd = TString::Format("b2hist(\"event\", %d)", run);
+  TString cmd = TString::Format("b2hist(%d, %d, \"event\")", run, win);
+  cout << cmd << endl;
+  gROOT->ProcessLine(cmd.Data());
+
+  cmd = TString::Format("b2hist(%d, %d, \"bg\")", run, win);
   cout << cmd << endl;
   gROOT->ProcessLine(cmd.Data());
 

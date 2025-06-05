7b17a3d4179b0325de90c3912112d8b1111afe10
hanX3 <hanxingchi@gmail.com>
2025-06-05 17:28:59 +0800

    change build name

--- build_all/batch.sh
+++ build_all/batch.sh
@@ -5,11 +5,18 @@ max_jobs=120
 mkdir -p out
 
 while read run; do
-  # ./build $run > out/$run &
-  root -b -q "macro.cpp($run, 100)" &
-  root -b -q "macro.cpp($run, 200)" &
-  root -b -q "macro.cpp($run, 1000)" &
-  # root -b -q "draw_sample.cpp($run)" &
+  ./b100 $run > out/$run &
+  ./b200 $run > out/$run &
+  ./b1000 $run > out/$run &
+  # root -b -q "macro.cpp($run, 100)" > ./out/$run.100 &
+  # root -b -q "macro.cpp($run, 200)" > ./out/$run.200 &
+  # root -b -q "macro.cpp($run, 1000)" > ./out/$run.1000 &
+  # root -b -q "draw_sample.cpp($run, 100)" &
+  # root -b -q "draw_sample.cpp($run, 200)" &
+  # root -b -q "draw_sample.cpp($run, 1000)" &
+  # root -b -q "draw_sample_no_bg.cpp($run, 100)" &
+  # root -b -q "draw_sample_no_bg.cpp($run, 200)" &
+  # root -b -q "draw_sample_no_bg.cpp($run, 1000)" &
 
   while (( $(jobs -p | wc -l) >= $max_jobs )); do
     wait -n

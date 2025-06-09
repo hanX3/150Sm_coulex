973fe1b8590886826497088b66174cb204e43125
hanX3 <hanxingchi@gmail.com>
2025-06-09 11:03:08 +0800

    add

--- check_ts_align/batch.sh
+++ check_ts_align/batch.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/bash
 
-max_jobs=110
+max_jobs=150
 
 mkdir -p out
 
 while read run; do
-  ./build $run > out/$run &
+  ./btf1 $run > out/$run &
+  ./btf2 $run > out/$run &
 
   while (( $(jobs -p | wc -l) >= $max_jobs )); do
     wait -n
--- check_ts_align/main.cpp
+++ check_ts_align/main.cpp
@@ -22,7 +22,7 @@ int main(int argc, char const *argv[])
   TString file_in = TString::Format("../rootfile/data_C2_%04d_%04d.root", run, run);
   std::cout << "analysis " << file_in << std::endl;
   
-  TString file_out = TString::Format("../rootfile/data%04d_align_%dns.root", run, TIMEWINDOW);
+  TString file_out = TString::Format("../rootfile/data%04d_align_%dns_%s.root", run, TIMEWINDOW, TF);
 
   TStopwatch timer;
   timer.Start();

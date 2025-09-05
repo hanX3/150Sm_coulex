eb239eb420cd4a79b3e3e60befd176b5fb44c0e3
hanX3 <hanxingchi@gmail.com>
2025-09-05 15:13:10 +0800

    batch

---
+++ s3_energy_attenuation/batch_draw_sample.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/bash
+
+max_jobs=120
+
+for i in $(seq -w 1 24);
+do
+  mkdir -p fig/ring${i}
+done
+
+for i in $(seq -w 1 32);
+do
+  mkdir -p fig/sector${i}
+done
+
+#
+while read run; do
+  root -b -q "draw_sample.cpp($run)" &
+
+  while [ "$(jobs -p | wc -l)" -ge "$max_jobs" ]; do
+    wait -n
+  done
+done < "./run.txt"

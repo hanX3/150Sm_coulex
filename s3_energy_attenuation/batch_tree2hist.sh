50df0eef6cecfd021ad908295ddce730093a79ea
hanX3 <hanxingchi@gmail.com>
2025-09-05 14:58:58 +0800

    batch

---
+++ s3_energy_attenuation/batch_tree2hist.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+max_jobs=120
+
+while read run; do
+  root -b -q "tree2hist.cpp($run)" &
+
+  while [ "$(jobs -p | wc -l)" -ge "$max_jobs" ]; do
+    wait -n
+  done
+done < "./run.txt"

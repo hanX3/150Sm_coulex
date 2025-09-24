7d232d4c0d51c15f4fef1fbf52a0e05988215dcd
hanX3 <hanxingchi@gmail.com>
2025-09-24 15:20:42 +0800

    delete other build

--- build_s3/batch.sh
+++ build_s3/batch.sh
@@ -5,9 +5,7 @@ max_jobs=120
 mkdir -p out
 
 while read run; do
-  ./build_no_s3att_no_s3cor $run > out/$run.txt &
-  ./build_no_s3cor $run > out/$run.txt &
-  # ./build $run > out/$run.txt &
+  ./build $run > out/$run.txt &
 
   while (( $(jobs -p | wc -l) >= $max_jobs )); do
     wait -n

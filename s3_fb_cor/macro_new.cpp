fb5cf3bd9a5ec369aba6c4a11d873359d3db0c48
hanX3 <hanxingchi@gmail.com>
2025-09-25 18:00:31 +0800

    fix

--- s3_fb_cor/macro_new.cpp
+++ s3_fb_cor/macro_new.cpp
@@ -1,10 +1,10 @@
 //
 void macro_new(int run)
 {
-  TString cmd_ring = TString::Format("root -b -q 'correction_ring_new.cpp(%d, %f, %f, %f)'", run, 5000., 80000., 0.6);
+  TString cmd_ring = TString::Format("root -b -q 'correction_ring_new.cpp(%d, %f, %f, %f)'", run, 200., 100000., 0.6);
   gSystem->Exec(cmd_ring);
 
-  TString cmd_sector = TString::Format("root -b -q 'correction_sector_new.cpp(%d, %f, %f, %f)'", run, 5000., 80000., 0.6);
+  TString cmd_sector = TString::Format("root -b -q 'correction_sector_new.cpp(%d, %f, %f, %f)'", run, 800., 100000., 0.6);
   gSystem->Exec(cmd_sector);
 
   gSystem->Exec("wait");

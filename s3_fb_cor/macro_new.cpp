7222130021dba1d5423d565ab5c3fd1f88e4fab2
hanX3 <hanxingchi@gmail.com>
2025-09-30 14:49:53 +0800

    set different rob par for diff run

--- s3_fb_cor/macro_new.cpp
+++ s3_fb_cor/macro_new.cpp
@@ -1,10 +1,30 @@
 //
 void macro_new(int run)
 {
-  TString cmd_ring = TString::Format("root -b -q 'correction_ring_new.cpp(%d, %f, %f, %f)'", run, 200., 100000., 0.6);
+  //
+  TString cmd_ring;
+  if(run>=472 && run<=605){
+    cmd_ring = TString::Format("root -b -q 'correction_ring_new.cpp(%d, %f, %f, %f)'", run, 200., 100000., 0.6);
+  }
+  if(run>=616 && run<=674){
+    cmd_ring = TString::Format("root -b -q 'correction_ring_new.cpp(%d, %f, %f, %f)'", run, 200., 100000., 0.8);
+  }
+  if(run>=680 && run<=718){
+    cmd_ring = TString::Format("root -b -q 'correction_ring_new.cpp(%d, %f, %f, %f)'", run, 200., 100000., 0.6);
+  }
   gSystem->Exec(cmd_ring);
 
-  TString cmd_sector = TString::Format("root -b -q 'correction_sector_new.cpp(%d, %f, %f, %f)'", run, 800., 100000., 0.6);
+  //
+  TString cmd_sector;
+  if(run>=472 && run<=605){
+    cmd_sector = TString::Format("root -b -q 'correction_sector_new.cpp(%d, %f, %f, %f)'", run, 800., 100000., 0.6);
+  }
+  if(run>=616 && run<=674){
+    cmd_sector = TString::Format("root -b -q 'correction_sector_new.cpp(%d, %f, %f, %f)'", run, 800., 100000., 0.8);
+  }
+  if(run>=680 && run<=718){
+    cmd_sector = TString::Format("root -b -q 'correction_sector_new.cpp(%d, %f, %f, %f)'", run, 800., 100000., 0.6);
+  }
   gSystem->Exec(cmd_sector);
 
   gSystem->Exec("wait");

7c11cb89f13d9244e6cee3075ef980691a15a6f7
hanX3 <hanxingchi@gmail.com>
2025-10-16 23:25:37 +0800

    add src

---
+++ doppler_optimize/op_s3_phi/src_raw/set.h
@@ -0,0 +1,13 @@
+#ifndef SET_H
+#define SET_H
+
+constexpr int GENUM = 64;
+constexpr double TARGETTHICKNESS = 1.6; //um
+
+constexpr double PHIOFFSET = 0;
+
+#define CONFIG "b"
+
+// #define TW
+
+#endif

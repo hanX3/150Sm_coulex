d04d2633d52ddfcd8acaa63f7722294718baeff5
hanX3 <hanxingchi@gmail.com>
2025-10-20 16:15:56 +0800

    d2hist

--- doppler_optimize/op_spider_cut/d2hist.cpp
+++ doppler_optimize/op_spider_cut/d2hist.cpp
@@ -39,37 +39,37 @@ void d2hist(int run, int cut)
       for(auto &str_e : v_str_e){
         // ge all, spider ring
         TH1D h1(Form("%s_%s_ge_all_spider_ring%d",(*it).c_str(),str_e.c_str(),i), "", 4096, 0, 4096);
-        tr->Project(h1.GetName(), str_e.c_str(), Form("flag_spider_p && !flag_spider_p && rp==%d",i));
+        tr->Project(h1.GetName(), str_e.c_str(), Form("flag_spider_p && !flag_s3_p && rp==%d",i));
         h1.Write();
 
         // ge ring, spider ring
         TH1D h3(Form("%s_%s_ge_ring3_spider_ring%d",(*it).c_str(),str_e.c_str(),i), "", 4096, 0, 4096);
-        tr->Project(h3.GetName(), str_e.c_str(), Form("flag_spider_p && !flag_spider_p && rp==%d && rg==3",i));
+        tr->Project(h3.GetName(), str_e.c_str(), Form("flag_spider_p && !flag_s3_p && rp==%d && rg==3",i));
         h3.Write();
 
         // ge ring, spider ring
         TH1D h4(Form("%s_%s_ge_ring4_spider_ring%d",(*it).c_str(),str_e.c_str(),i), "", 4096, 0, 4096);
-        tr->Project(h4.GetName(), str_e.c_str(), Form("flag_spider_p && !flag_spider_p && rp==%d && rg==4",i));
+        tr->Project(h4.GetName(), str_e.c_str(), Form("flag_spider_p && !flag_s3_p && rp==%d && rg==4",i));
         h4.Write();
 
         // ge ring, spider ring
         TH1D h5(Form("%s_%s_ge_ring5_spider_ring%d",(*it).c_str(),str_e.c_str(),i), "", 4096, 0, 4096);
-        tr->Project(h5.GetName(), str_e.c_str(), Form("flag_spider_p && !flag_spider_p && rp==%d && rg==5",i));
+        tr->Project(h5.GetName(), str_e.c_str(), Form("flag_spider_p && !flag_s3_p && rp==%d && rg==5",i));
         h5.Write();
 
         // ge single, spider ring
         TH1D h32(Form("%s_%s_ge_ring3_sector2_spider_ring%d",(*it).c_str(),str_e.c_str(),i), "", 4096, 0, 4096);
-        tr->Project(h32.GetName(), str_e.c_str(), Form("flag_spider_p && !flag_spider_p && rp==%d && rg==3 && sg==2",i));
+        tr->Project(h32.GetName(), str_e.c_str(), Form("flag_spider_p && !flag_s3_p && rp==%d && rg==3 && sg==2",i));
         h32.Write();
 
         // ge single, spider ring
         TH1D h41(Form("%s_%s_ge_ring4_sector1_spider_ring%d",(*it).c_str(),str_e.c_str(),i), "", 4096, 0, 4096);
-        tr->Project(h41.GetName(), str_e.c_str(), Form("flag_spider_p && !flag_spider_p && rp==%d && rg==4 && sg==1",i));
+        tr->Project(h41.GetName(), str_e.c_str(), Form("flag_spider_p && !flag_s3_p && rp==%d && rg==4 && sg==1",i));
         h41.Write();
 
         // ge single, spider ring
         TH1D h52(Form("%s_%s_ge_ring5_sector2_spider_ring%d",(*it).c_str(),str_e.c_str(),i), "", 4096, 0, 4096);
-        tr->Project(h52.GetName(), str_e.c_str(), Form("flag_spider_p && !flag_spider_p && rp==%d && rg==5 && sg==2",i));
+        tr->Project(h52.GetName(), str_e.c_str(), Form("flag_spider_p && !flag_s3_p && rp==%d && rg==5 && sg==2",i));
         h52.Write();
       }
     }

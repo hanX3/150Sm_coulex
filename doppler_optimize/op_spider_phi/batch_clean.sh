c8321b1822a7c9050ef45665c30f775a8a6895c5
hanX3 <hanxingchi@gmail.com>
2025-10-21 22:15:19 +0800

    op_spider_phi

--- doppler_optimize/op_spider_phi/batch_clean.sh
+++ doppler_optimize/op_spider_phi/batch_clean.sh
@@ -1,10 +1,3 @@
 #!/usr/bin/bash
 
-max_jobs=120
-
-l_phi_offset=(0 5 10 330 335 340 345 350 355)
-
-for phi_offset in "${l_phi_offset[@]}"; do
-  name=$(printf "%03ddeg" ${phi_offset}) 
-  rm -rf try/${name}
-done
+rm -rf try/*

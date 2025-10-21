35490e7e7b6ef0a7cf29537c9ce14d00e0cbb5ac
hanX3 <hanxingchi@gmail.com>
2025-10-21 21:05:57 +0800

    update

--- doppler_optimize/op_s3_cut/batch_clean.sh
+++ doppler_optimize/op_s3_cut/batch_clean.sh
@@ -1,10 +1,3 @@
 #!/usr/bin/bash
 
-max_jobs=120
-
-l_cut=(5000 10000 15000 20000 25000 30000 35000 40000 45000 50000)
-
-for cut in "${l_cut[@]}"; do
-  name=$(printf "%05dkeV" ${cut}) 
-  rm -rf try/${name}
-done
+rm -rf try/*

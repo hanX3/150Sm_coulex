98d9c0ddbf780a4b98f36dfa6bb38fbac638e72c
hanX3 <hanxingchi@gmail.com>
2025-10-23 11:19:01 +0800

    batch

---
+++ doppler/evaluation/batch_d2hist_simple.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+max_jobs=160
+
+mkdir -p terminal_cout
+
+while read run; do
+  root -b -q "d2hist_simple.cpp($run, 100, 300)" &
+  
+  while (( $(jobs -p | wc -l) >= $max_jobs )); do
+    wait -n
+  done
+done < "../../pars/run.txt"

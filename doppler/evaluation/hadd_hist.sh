5d4a211f9e984d0d96ef4ac599b87ff9d51a4aa5
hanX3 <hanxingchi@gmail.com>
2025-10-30 16:59:22 +0800

    add

--- doppler/evaluation/hadd_hist.sh
+++ doppler/evaluation/hadd_hist.sh
@@ -5,7 +5,14 @@ if [ $# -ne 1 ]; then
     exit 1
 fi
 
-#
+# p1
+rm -f ../../rootfile/data0616_0674_doppler_${1}ns_jump_300ns_hist.root 
+hadd ../../rootfile/data0616_0674_doppler_${1}ns_jump_300ns_hist.root ../../rootfile/data061[6-9]_doppler_${1}ns_jump_300ns_hist.root ../../rootfile/data06[2-6]?_doppler_${1}ns_jump_300ns_hist.root ../../rootfile/data067[1-4]_doppler_${1}ns_jump_300ns_hist.root
+
+rm -f ../../rootfile/doppler_${1}ns_p1_hist.root
+mv ../../rootfile/data0616_0674_doppler_${1}ns_jump_300ns_hist.root ../../rootfile/doppler_${1}ns_p1_hist.root
+
+# p2
 rm -f ../../rootfile/data0472_0549_doppler_${1}ns_jump_300ns_hist.root 
 hadd ../../rootfile/data0472_0549_doppler_${1}ns_jump_300ns_hist.root ../../rootfile/data04??_doppler_${1}ns_jump_300ns_hist.root ../../rootfile/data05[0-4]?_doppler_${1}ns_jump_300ns_hist.root      
 
@@ -15,19 +22,8 @@ hadd ../../rootfile/data0550_0605_doppler_${1}ns_jump_300ns_hist.root ../../root
 rm -f ../../rootfile/data0472_0605_doppler_${1}ns_jump_300ns_hist.root
 hadd ../../rootfile/data0472_0605_doppler_${1}ns_jump_300ns_hist.root ../../rootfile/data0472_0549_doppler_${1}ns_jump_300ns_hist.root ../../rootfile/data0550_0605_doppler_${1}ns_jump_300ns_hist.root
 
-rm -f ../../rootfile/data0616_0674_doppler_${1}ns_jump_300ns_hist.root 
-hadd ../../rootfile/data0616_0674_doppler_${1}ns_jump_300ns_hist.root ../../rootfile/data061[6-9]_doppler_${1}ns_jump_300ns_hist.root ../../rootfile/data06[2-6]?_doppler_${1}ns_jump_300ns_hist.root ../../rootfile/data067[1-4]_doppler_${1}ns_jump_300ns_hist.root
-
 rm -f ../../rootfile/data0680_0718_doppler_${1}ns_jump_300ns_hist.root
 hadd ../../rootfile/data0680_0718_doppler_${1}ns_jump_300ns_hist.root ../../rootfile/data06[8-9]?_doppler_${1}ns_jump_300ns_hist.root ../../rootfile/data070?_doppler_${1}ns_jump_300ns_hist.root ../../rootfile/data071?_doppler_${1}ns_jump_300ns_hist.root
 
-rm -f ../../rootfile/data0472_0718_doppler_${1}ns_jump_300ns_hist.root
-hadd ../../rootfile/data0472_0718_doppler_${1}ns_jump_300ns_hist.root ../../rootfile/data0472_0605_doppler_${1}ns_jump_300ns_hist.root ../../rootfile/data0616_0674_doppler_${1}ns_jump_300ns_hist.root ../../rootfile/data0680_0718_doppler_${1}ns_jump_300ns_hist.root
-
-# low
-rm -f ../../rootfile/doppler_${1}ns_p1_hist.root
-mv ../../rootfile/data0616_0674_doppler_${1}ns_jump_300ns_hist.root ../../rootfile/doppler_${1}ns_p1_hist.root
-
-# high
 rm -f ../../rootfile/doppler_${1}ns_p2_hist.root
 hadd ../../rootfile/doppler_${1}ns_p2_hist.root ../../rootfile/data0472_0605_doppler_${1}ns_jump_300ns_hist.root ../../rootfile/data0680_0718_doppler_${1}ns_jump_300ns_hist.root

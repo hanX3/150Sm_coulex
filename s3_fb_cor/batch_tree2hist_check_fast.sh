9af6341e703d44f72fdab4804dc48c9913a0f0a2
hanX3 <hanxingchi@gmail.com>
2025-12-02 17:13:41 +0800

    add

--- s3_fb_cor/batch_tree2hist_check_fast.sh
+++ s3_fb_cor/batch_tree2hist_check_fast.sh
@@ -3,6 +3,7 @@
 max_jobs=120
 
 while read run; do
+  mkdir -p fig/$(printf "%04d" $run) 
   root -b -q "tree2hist_check_fast.cpp($run)" &
 
   while (( $(jobs -p | wc -l) >= $max_jobs )); do
--- s3_fb_cor/draw_sample_rs_th2.cpp
+++ s3_fb_cor/draw_sample_rs_th2.cpp
@@ -1,15 +1,13 @@
 //
 void draw_sample_rs_th2(int run, int ring, int sector)
 {
-  TString cut_str = Form("s3_ring_id==%d && s3_sector_id==%d", ring, sector);
-
   //
   TFile *fi = TFile::Open(Form("../rootfile/si/data%04d_build_s3_200ns.root",run));
   TTree *tr_event = (TTree*)fi->Get("tr_event");
 
   TH2D *hh = new TH2D("hh", "", 1000, 0, 100000, 1000, 0, 100000);
 
-  tr_event->Draw("s3_sector_energy:s3_ring_energy>>hh", cut_str.Data(), "goff", tr_event->GetEntries());
+  tr_event->Draw("s3_sector_energy:s3_ring_energy>>hh", "", "goff", tr_event->GetEntries());
   hh->SetTitle("th2 between sector and ring");
   hh->GetXaxis()->SetTitle("energy [keV]");
   hh->GetYaxis()->SetTitle("energy [keV]");
@@ -26,6 +24,8 @@ void draw_sample_rs_th2(int run, int ring, int sector)
   TTree *tr_event_new = (TTree*)fi_new->Get("tr_event");
 
   TH2D *hh_new = new TH2D("hh_new", "", 1000, 0, 100000, 1000, 0, 100000);
+
+  TString cut_str = Form("s3_ring_id==%d && s3_sector_id==%d", ring, sector);
 
   tr_event_new->Draw("s3_sector_energy:s3_ring_energy>>hh_new", cut_str.Data(), "goff", tr_event_new->GetEntries());
   hh_new->SetTitle("th2 between sector and ring new");
